8340e96c8dddf9731bdb95b260ddf03a8e0495d3
Sarah Jacob <smjacob91@gmail.com>
2018-07-18 14:31:28 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
-# code here!
+require 'pry'
+
+class School
+  
+  attr_accessor :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(student_name, student_grade)
+    if @roster.include?(student_name)
+      return @roster
+    else
+      @roster << student_name
+    end
+  end
+  
+end
+
+
